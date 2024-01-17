309fe7185b7e0fad551112e88f7ffddde1f9c6d8
dprdeveloper <davidpozorojas@gmail.com>
2024-01-17 09:18:58 +0100

    Implement animation in image splash

--- app/src/main/java/es/dpr/marvelsampleapp/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/es/dpr/marvelsampleapp/ui/screens/splash/SplashScreen.kt
@@ -1,5 +1,10 @@
 package es.dpr.marvelsampleapp.ui.screens.splash
 
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,8 +12,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import es.dpr.marvelsampleapp.R
@@ -16,6 +24,13 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(onSplashFinish: ()->Unit) {
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 1.1f,
+        animationSpec = infiniteRepeatable(tween(1000), RepeatMode.Reverse),
+        label = "scale"
+    )
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -25,7 +40,13 @@ fun SplashScreen(onSplashFinish: ()->Unit) {
     ) {
         Image(
             painter = painterResource(id = R.drawable.marvel),
-            contentDescription = null
+            contentDescription = null,
+            modifier = Modifier
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
+                    transformOrigin = TransformOrigin.Center
+            }
         )
     }
     LaunchedEffect(Unit){
